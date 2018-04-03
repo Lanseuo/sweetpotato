2083d675dee0d63f8e6f62ccd71bdb9c2b1bce94
Lucas Hild <contact@lucas-hild.de>
2018-04-03 15:52:25 +0200

    Fix api base url

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const apiURL = process.env.REACT_APP_API_URL || 'http://localhost:5000'
+export const apiURL = process.env.REACT_APP_API_URL || ''
 
 export default () => {
   return axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000'
+    baseURL: process.env.REACT_APP_API_URL || ''
   })
 }
